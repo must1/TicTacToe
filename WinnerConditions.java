7552700116ad40637e5fe17214004b7b4fa8a7dd
must1 <must.janik@gmail.com>
2017-12-23 17:30:19 +0100

    update

--- WinnerConditions.java
+++ WinnerConditions.java
@@ -4,9 +4,9 @@ public class WinnerConditions {
     char[][] board = PrintBoard.board;
 
 
-    public boolean VerticalWinner(int col) {
+    /*public boolean VerticalWinner(int col) {
         if (board[0][col - 1] == board[1][col - 1] &&
-            board[1][col - 1] == board[2][col - 1]) {
+                board[1][col - 1] == board[2][col - 1]) {
             return false;
         }
         return true;
@@ -15,7 +15,7 @@ public class WinnerConditions {
 
     public boolean HorizontalWinner(int row) {
         if (board[row - 1][0] == board[row - 1][1] &&
-            board[row - 1][1] == board[row - 1][2]) {
+                board[row - 1][1] == board[row - 1][2]) {
             return false;
         }
         return true;
@@ -23,8 +23,38 @@ public class WinnerConditions {
 
 
     public boolean DiagonalWinner() {
-        if (board[0][0] == board[1][1] &&
-            board[1][1] == board[2][2])
+        if (board[0][0] == 'X' || board[0][0] == 'O') {
+            if (board[0][0] == board[1][1] &&
+                    board[1][1] == board[2][2])
+                return false;
+        }
+        if (board[0][1] == 'X' || board[0][1] == 'O') {
+            if (board[0][1] == board[1][1] &&
+                    board[1][1] == board[2][0])
+                return false;
+        }
+
+
+        return true;
+    }*/
+
+    public boolean Winner() {
+
+        if (board[0][0] != '_' && board[0][0] == board[0][1] && board[0][0] == board[0][2])
+            return false;
+        if (board[1][0] != '_' && board[1][0] == board[1][1] && board[1][0] == board[1][2])
+            return false;
+        if (board[2][0] != '_' && board[2][0] == board[2][1] && board[2][0] == board[2][2])
+            return false;
+        if (board[0][0] != '_' && board[0][0] == board[1][0] && board[0][0] == board[2][0])
+            return false;
+        if (board[0][1] != '_' && board[0][1] == board[1][1] && board[0][1] == board[2][1])
+            return false;
+        if (board[0][2] != '_' && board[0][2] == board[1][2] && board[0][2] == board[2][2])
+            return false;
+        if (board[0][0] != '_' && board[0][0] == board[1][1] && board[0][0] == board[2][2])
+            return false;
+        if (board[0][2] != '_' && board[0][2] == board[1][1] && board[0][2] == board[2][0])
             return false;
 
         return true;
