44497aa65b3367b0b82f486fcbd4c7c51ff731c2
must1 <must.janik@gmail.com>
2017-12-25 22:40:13 +0100

    update

--- Game.java
+++ Game.java
@@ -13,13 +13,11 @@ class Game {
 
     void StartGame() {
 
-
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '_';
             }
         }
-
 
         map.printBoard(board);
         while (conditionsChecker.checkIfWinner(board)) {
