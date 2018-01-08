85269574e6d7a8e33e07146a5fb4de72413aac7c
must1 <must.janik@gmail.com>
2018-01-08 21:00:06 +0100

    Code was reformatted

--- BoardPrinter.java
+++ BoardPrinter.java
@@ -5,14 +5,19 @@ public class BoardPrinter {
     public void printBoard(char[][] board) {
 
         System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++)
+        {
             System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
+
+            for (int j = 0; j < 3; j++)
+            {
 
                 System.out.print(board[i][j] + " | ");
 
             }
+
             System.out.println();
+
         }
         System.out.println("-------------");
     }
