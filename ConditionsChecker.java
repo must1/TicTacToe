40a3808d2601a366f77d9d6deac1cd2c7dc24b06
must1 <must.janik@gmail.com>
2018-01-08 20:58:13 +0100

    Modify checkIfWinner method

--- ConditionsChecker.java
+++ ConditionsChecker.java
@@ -2,41 +2,46 @@ package tictactoe;
 
 public class ConditionsChecker {
 
-    public boolean checkIfWinner(char[][] board) {
-
-        if (board[0][0] != '_' && board[0][0] == board[0][1] && board[0][0] == board[0][2])
-            return false;
-        if (board[1][0] != '_' && board[1][0] == board[1][1] && board[1][0] == board[1][2])
-            return false;
-        if (board[2][0] != '_' && board[2][0] == board[2][1] && board[2][0] == board[2][2])
-            return false;
-        if (board[0][0] != '_' && board[0][0] == board[1][0] && board[0][0] == board[2][0])
-            return false;
-        if (board[0][1] != '_' && board[0][1] == board[1][1] && board[0][1] == board[2][1])
-            return false;
-        if (board[0][2] != '_' && board[0][2] == board[1][2] && board[0][2] == board[2][2])
-            return false;
+    public boolean checkIfWinner(char[][] board)
+    {
+        for(int i = 0; i <= board.length-1; i++)
+        {
+            if (board[i][0] != '_' && board[i][0] == board[i][1] && board[i][0] == board[i][2])
+                return true;
+        }
+        for(int i = 0; i <= board.length-1; i++)
+        {
+            if (board[0][i] != '_' && board[0][i] == board[1][i] && board[0][i] == board[2][i])
+                return true;
+        }
         if (board[0][0] != '_' && board[0][0] == board[1][1] && board[0][0] == board[2][2])
-            return false;
+        {
+            return true;
+        }
         if (board[0][2] != '_' && board[0][2] == board[1][1] && board[0][2] == board[2][0])
-            return false;
-
-        return true;
-    }
-
-    public boolean checkIfOccupied(int row, int col, char[][] board) {
-        if (board[row - 1][col - 1] == 'X' ||
-                board[row - 1][col - 1] == 'O') {
-            System.err.println("The place is taken");
+        {
             return true;
         }
         return false;
     }
 
+    public boolean checkIfOccupied(int row, int col, char[][] board) {
+        if (board[row - 1][col - 1] == 'X' ||
+            board[row - 1][col - 1] == 'O') {
+
+            System.err.println("The place is taken");
+            return true;
+
+        }
+        return false;
+    }
+
     public boolean checkIfOutOfBoard(int row, int col) {
-        if (row > 3 || col > 3) {
+        if (row > 3 || row < 1 || col > 3 || col < 1) {
+
             System.err.println("You've inputed place, which is out of the board!\nTry again!");
             return true;
+
         }
         return false;
     }
